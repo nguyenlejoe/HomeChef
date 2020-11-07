6ac32b9ee25893245c91b67b25493babc7579f78
semileeBCIT <55604874+semileeBCIT@users.noreply.github.com>
2020-11-07 10:54:58 -0800

    fixed AddInput, building pages

---
+++ .history/app/src/App_20201106221238.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import Pages from './pages/Checkout';
+
+function App() {
+  return (
+    <Pages />
+  );
+}
+
+export default App;
