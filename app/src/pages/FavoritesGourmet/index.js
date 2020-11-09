9632af733ab34c8f4cf9a30e68a42df0475e8bb8
milanjot <milannagra@milans-MacBook-Pro.local>
2020-11-08 23:46:27 -0800

    Creating favorites page

---
+++ app/src/pages/FavoritesGourmet/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Favorites.scss'
+import foodcover from '../../comps/FoodDisplayCover';
+import ConfirmOrder from '../../comps/ConfirmOrder';
+import NavBarChef from '../../comps/NavBarChef';
+
+
+
+
+
+export default function Home() {
+  return<div>
+
+  </div>
+
+}
