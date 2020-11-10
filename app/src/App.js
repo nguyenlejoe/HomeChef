1a099d8104d1dba92c5e14eb21817cffc99cd819
nguyenlejoe <joe_le.1@hotmail.com>
2020-11-09 20:49:48 -0800

    Troubles with github, sorry henry

--- app/src/App.js
+++ app/src/App.js
@@ -16,13 +16,12 @@ import CreateItem from './pages/CreateItem';
 import OrderConfirmation from './pages/OrderConfirmation';
 import Checkout from './pages/Checkout';
 import ChefReview from './pages/ChefReview';
-import ChefOrdersConfirmed from './pages/ChefOrdersConfirmed';
+import ChefOrdersConfirm from './pages/ChefOrders-Confirmed';
 import AddBalance from './pages/AddBalance';
 import CurrentOrderPage from './pages/CurrentOrderPage';
 import OrderHistoryPage from './pages/OrderHistoryPage';
 // import DishDescription from './pages/DishDescription';
-import ChefOrders from './pages/ChefOrders-Pending';
-import ChefOrdersconfirm from './pages/ChefOrders-Confirmed';
+import ChefOrdersPending from './pages/ChefOrders-Pending';
 import GourmetFavorites from './pages/FavoritesGourmet';
 import ChefMyAccount from './pages/ChefMyAccount'
 
@@ -34,10 +33,10 @@ function App() {
   return (
     <Router>
       <Switch>
-      <Route path="/" exact component={ChefMyAccount}></Route>
-      <Route path="/" exact component={ChefOrdersconfirm}></Route>
-      <Route path="/" exact component={GourmetFavorites}></Route>
-      {/* <Route path="/" exact component={Login}></Route> */}
+      <Route path="/ChefMyAccount" exact component={ChefMyAccount}></Route>
+      <Route path="/ChefOrdersConfirm" exact component={ChefOrdersConfirm}></Route>
+      <Route path="/GourmetFavorites" exact component={GourmetFavorites}></Route>
+      <Route path="/" exact component={Login}></Route>
       <Route path="/CreateAccountChef" exact component={CreateAccountPageChef}></Route>
       <Route path="/DashBoardGourmet" exact component={DashBoardGourmet}></Route>
       <Route path="/CreateAccountGourmet" exact component={CreateAccountGourmet}></Route>
@@ -47,7 +46,6 @@ function App() {
       <Route path="/OrderConfirmation" exact component={OrderConfirmation}></Route>
       <Route path="/Checkout" exact component={Checkout}></Route>
       <Route path="/ChefReview" exact component={ChefReview}></Route>
-      <Route path="/ChefOrdersConfirmed" exact component={ChefOrdersConfirmed}></Route>
       <Route path="/AddBalance" exact component={AddBalance}></Route>
       <Route path="/CurrentOrderPage" exact component={CurrentOrderPage}></Route>
       <Route path="/OrderHistoryPage" exact component={OrderHistoryPage}></Route>
