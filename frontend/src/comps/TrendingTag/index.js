f951524b8b823bf0b59de11236fa49a04fc734a7
=Yunus Emre <=yeagacdan@gmail.com>
2020-11-11 03:18:37 -0800

    ss

--- frontend/src/comps/TrendingTag/index.js
+++ frontend/src/comps/TrendingTag/index.js
@@ -4,24 +4,27 @@ import styled, {css} from 'styled-components';
 const Trendingtagcont = styled.div`
 display:flex;
 align-items:center;
-height: 34px;
-width: 124px;
-background: #6FCF97;
-justify-content:center;
+height: ${props =>props.height ? props.height : '28px'} ;
+width: ${props =>props.width ? props.width : '100px'};
+color:${props =>props.textColor ? props.textColor : '#333333'}; 
+background: ${props =>props.bgcolor ? props.bgcolor : '#6FCF97'};
+font-size: ${props =>props.fontSize ? props.fontSize : '12px'};
+justify-content:start;
 border-radius: 39px;
+margin: 5px;
+padding-left: 3px;
 
-
-`
+`;
 const Trendtag = styled.div`
 margin:4px;
-`
+`;
 
 const Trendingtxt = styled.div`
 margin:4px;
-`
-const TrendingTag = ({text}) => {
+`;
+const TrendingTag = ({text, color, bgcolor, width, height, fontSize}) => {
     
-    return <Trendingtagcont>
+    return <Trendingtagcont color={color} bgcolor={bgcolor} width={width} height={height} fontSize={fontSize}>
         <Trendtag> <img src="/tag.svg"/>
         </Trendtag>
         <Trendingtxt>{text}</Trendingtxt>
