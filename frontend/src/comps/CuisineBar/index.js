3e5efc1611d3e9295ce3bff96a17b5e12ced4570
=Yunus Emre <=yeagacdan@gmail.com>
2020-11-14 23:54:14 -0800

    Cuisine Bar fix

--- frontend/src/comps/CuisineBar/index.js
+++ frontend/src/comps/CuisineBar/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled, {css} from 'styled-components';
 import Button from '../MainButton';
+import Select from 'react-select';
+
+const optionCuisines= [
+    {value: 'turkish' , label: 'Turkish'},
+    {value: 'French' , label: 'French'},
+    {value: 'Greek' , label: 'Greek'},
+    {value: 'Indian' , label: 'Indian'},
+    {value: 'Iranian' , label: 'Iranian'},
+    {value: 'Chinese' , label: 'Chinese'},
+    {value: 'Italian' , label: 'Italian'},
+    {value: 'Japanese' , label: 'Japanese'},
+    {value: 'Mexican' , label: 'Mexican'},
+    {value: 'Korean' , label: 'Korean'},
+    {value: 'Taiwanese' , label: 'Taiwanese'},
+];
 
 const CuisineNav = styled.div`
+    width: 100%;
     display:flex;
     align-items: center;
     justify-content: space-between;
 `;
 
-const CuisineBar = () =>{
+export function CuisineBar() {
+
+    function customTheme(theme){
+        return{
+            ...theme,
+            colors: {
+                ...theme.colors,
+                primary25:'#69ECB1',
+                primary:'#24B574'
+            }
+        }
+    }
     return <CuisineNav>
         <Button width= "88px" height= "35px" radius="15px" fontSize= "14px" text="Filter" bgcolor= "#FFFFFF" textColor="#24B574"/>
-        <Button width= "135px" height= "44px" fontSize= "18px" text = "Cuisine" radius="20px"/>
+        
+        <Select
+            options={optionCuisines}
+            theme={customTheme}
+            placeholder='Cuisine'
+
+        />
         <Button width= "88px" height= "35px" radius="15px" fontSize= "14px" text="20.00" bgcolor= "#FFFFFF" textColor="#24B574"/>
     </CuisineNav>
 };
