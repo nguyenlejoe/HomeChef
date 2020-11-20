f39f34d90a98b692dd12bffde5d5d4aa0c2cd7f1
milanjot <milanjot@hotmail.ca>
2020-11-19 19:54:42 -0800

    Created Edit Menu Items page

---
+++ frontend/src/pages/EditMenuItems/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './EditMenuItems.scss';
+import BackButton from '../../comps/BackButton';
+import Avatar from '../../comps/Avatar'
+
+
+
+
+
+export default function Home() {
+  return<div>
+    <div className="backbutton">
+    <BackButton></BackButton>
+    </div>
+    
+    <div className="HeadingTxt">
+  <h3> Alex Ingy's Menu </h3>
+    </div>
+    
+  <div className="Avatar">
+    <div className="Avatar">
+      <Avatar></Avatar>
+    </div>
+    </div>
+  
+
+
+
+
+  </div>
+
+}
